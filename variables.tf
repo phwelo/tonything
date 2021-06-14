e24b01ae71dbe5e99cd0bdfb88b5498c17d3fc98
Tony Licavoli <6775563+tonytuba@users.noreply.github.com>
2021-06-14 18:20:08 -0400

    Last bit for now

--- variables.tf
+++ variables.tf
@@ -6,12 +6,11 @@ variable "ingress_cidr" {
     default = "0.0.0.0/0"
 }
 
+variable "egress_cidr" {
+    default = "0.0.0.0/0"
+}
+
 variable "vpc_id" {}
 variable "subnet_id" {}
 variable "profile" {}
 variable "region" {}
-
-variable "sec_grps" {
-    default = [module.ec2_instance.vpc_security_group_ids]
-
-}
