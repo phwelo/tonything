55f9e1afe62cdc08452e3741d7859894584acebf
Tony Licavoli <6775563+tonytuba@users.noreply.github.com>
2021-06-14 17:14:09 -0400

    trying out sg loop

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ data "aws_ami" "amazon_linux" {
   }
 }
 
-module "security_group" {
+module "security_group1" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
   name        = "codesamplesg1"
@@ -26,13 +26,25 @@ module "security_group" {
   egress_rules        = ["all-all"]
 }
 
+module "security_group2" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "~> 4.0"
+  name        = "codesamplesg2"
+  description = "Security group for codesample2"
+  vpc_id      = var.vpc_id
+  ingress_cidr_blocks = [var.ingress_cidr]
+  ingress_rules       = ["44444-tcp"]
+  egress_rules        = ["all-all"]
+}
+
 module "ec2_instance" {
-  source                 = "terraform-aws-modules/ec2-instance/aws"
-  version                = "~> 2.0"
+  for_each                    = module.ec2_instance.vpc_security_group_ids
+  source                      = "terraform-aws-modules/ec2-instance/aws"
+  version                     = "~> 2.0"
   name                        = "codesample1"
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
   subnet_id                   = var.subnet_id
-  vpc_security_group_ids      = [module.security_group.security_group_id]
+  vpc_security_group_ids      = [module.security_group.security_group1, module.security_group.security_group2]
   associate_public_ip_address = true
 }
