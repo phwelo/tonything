725e4aa47ba404c69a34c58fdfddc1c758010ecc
Daniel Agans <daniel.agans@vroom.com>
2021-06-14 16:26:14 -0400

    split out some

--- main.tf
+++ main.tf
@@ -1,51 +1,27 @@
-terraform {
-  required_version = ">= 0.12.6"
-
-  required_providers {
-    aws = ">= 2.65"
-  }
-}
-
-provider "aws" {
-  region = var.region
-  profile = var.profile
-}
-
 data "aws_vpc" "default" {
   default = true
 }
 
 data "aws_ami" "amazon_linux" {
   most_recent = true
-
   owners = ["amazon"]
-
   filter {
     name = "name"
-
-    values = [
-      "amzn-ami-hvm-*-x86_64-gp2",
-    ]
+    values = [var.ami_search]
   }
-
   filter {
     name = "owner-alias"
-
-    values = [
-      "amazon",
-    ]
+    values = ["amazon"]
   }
 }
 
 module "security_group" {
   source  = "terraform-aws-modules/security-group/aws"
   version = "~> 4.0"
-
   name        = "codesamplesg1"
   description = "Security group for codesample1"
   vpc_id      = var.vpc_id
-
-  ingress_cidr_blocks = ["0.0.0.0/0"]
+  ingress_cidr_blocks = [var.ingress_cidr]
   ingress_rules       = ["http-80-tcp", "all-icmp"]
   egress_rules        = ["all-all"]
 }
@@ -53,8 +29,6 @@ module "security_group" {
 module "ec2_instance" {
   source                 = "terraform-aws-modules/ec2-instance/aws"
   version                = "~> 2.0"
-
-
   name                        = "codesample1"
   ami                         = data.aws_ami.amazon_linux.id
   instance_type               = "t2.micro"
---
+++ variables.tf
@@ -0,0 +1,12 @@
+variable "ami_search" {
+    default = "amzn-ami-hvm-*-x86_64-gp2"
+}
+
+variable "ingress_cidr" {
+    default = "0.0.0.0/0"
+}
+
+variable "vpc_id" {}
+variable "subnet_id" {}
+variable "profile" {}
+variable "region" {}
